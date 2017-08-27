9242b69a0401bf3a55e3b89432a4cb00aea8a77b
= <=>
2017-08-27 19:50:08 +0900

    activity->hosting

--- hosting/urls.py
+++ hosting/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     url(r'^$', views.main_content, name='main'),
     url(r'^activity/detail/(?P<pk>\d+)/$', views.detail, name='detail'),
+    url(r'^hosting/$', views.host, name='hosting'),
+    url(r'^hosting/0/$', views.host0, name='host0'),
+    url(r'^hosting/1/$', views.host1, name='host1'),
+    url(r'^hosting/2/$', views.host2, name='host2'),
+	url(r'^hosting/4/(?P<pk>\d+)/$', views.host4, name='host4'),
+	url(r'^hosting/8/(?P<pk>\d+)/$', views.host8, name='host8'),
 ]
--- hosting/views.py
+++ hosting/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
-from .models import Activity, ActivitySchedule, ActivityLanguage, ActivityPicture, HashTag, ActivityReview, BookMark
+from django.shortcuts import render, redirect, get_object_or_404
+from django.template import loader, Context
+from django.http import HttpResponse
+from .models import ActivityInfo, ActivityPicture
+from .forms import ActivityForm, Host4AcivityForm, Host4ActivityPicture, Host4HashTagForm
 
 # Create your views here.
 def main_content(request) :
-    contents = Activity.objects.all();
+    contents = ActivityInfo.objects.all();
     pictures = ActivityPicture.objects.all();
     return render(request, 'hosting/content.html', {'contents' : contents, 'pictures' : pictures})
 
@@ -16,3 +19,51 @@ def detail(request, pk) :
     host = get_object_or_404(Host, user_id = activity.user_id)
     context = { "detail" : activity, "picture" : picture, "hashtags" : hash_tag, "reviews" : review, "host" : host, "schedules" : schedule}
     return render(request, 'hosting/activity_detail.html', context)
+
+def host(request) :
+    return render(request, 'hosting/startHosting.html', {})
+
+def host0(request) :
+    return render(request, 'hosting/host0.html', {})
+
+def host1(request):
+    return render(request,'hosting/host1.html',{})
+
+def host2(request):
+    if request.method=="POST":
+        form=ActivityForm(request.POST)
+        if form.is_valid():
+            new_activity=form.save(commit=False)
+            new_activity.save()
+            return redirect('host4', pk=new_activity.pk)
+    else:
+        form=ActivityForm()
+    return render(request,'hosting/host2.html',{'form':form})
+
+def host4(request, pk) :
+    activity = get_object_or_404(ActivityInfo, pk=pk)
+    if request.method=="POST" :
+        #needed forms : Host4AcivityForm Host4HashTagForm Host4ActivityPicture
+        form = Host4AcivityForm(request.POST, instance=activity)
+        hash_tag = Host4HashTagForm(request.POST)
+        pictures = Host4ActivityPicture(request.POST)
+        if form.is_valid() :
+            #form save
+            activity = form.save(commit = False)
+            new_hash = hash_tag.save(commit = False)
+            new_picture = pictures.save(commit = False)
+            activity.save()
+            new_hash.save()
+            new_picture.save()
+            return redirect('host5', pk=activity.pk)
+    else :
+        #needed forms
+        form = Host4AcivityForm(instance = activity)
+        hash_tag = Host4HashTagForm()
+        pictures = Host4ActivityPicture()
+    return render(request, 'hosting/host4.html', {'activity' : activity, 'hash_tag' : hash_tag, 'pictures' : pictures})
+
+def host8(request,pk):
+    activity=get_object_or_404(ActivityInfo,pk=pk)
+    context={"new_activity":activity}
+    return render(request,'hosting/host8.html',context)
