d6c6cf09d3cc3efdd3191c770e79567a0699eb42
= <=>
2017-08-27 19:54:29 +0900

    add Host Model

--- member/models.py
+++ member/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Host(models.Model) :
+    user_id = models.ForeignKey(User, primary_key=True)
+    picture = models.URLField(null=True, blank = True)
+    university = models.CharField(max_length = 50, blank = True, null=True)
+    location = models.CharField(max_length = 175, blank = True, null=True)
+    introduction = models.CharField(max_length = 500, blank = True, null=True)
+    universityIntroduction = models.CharField(max_length = 500, blank = True, null=True)
+    totalRating = models.FloatField(default = 0.00)
